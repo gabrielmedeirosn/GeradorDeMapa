56c8dc15d19f7c8ea74dc57751e997d3c012217f
gabrielmedeirosn <gabrielmedeirosn@gmail.com>
2025-07-15 02:28:44 +0000

    Codigo V1.0

---
+++ IMG.h
@@ -0,0 +1,32 @@
+#ifndef IMG_H
+#define IMG_H
+#include <iostream>
+#include "COR.h"
+using namespace std;
+
+
+class Image{
+
+private:
+    int height, width;
+    Colors* pixels; //definidas em tempo de execução -  array é alocado dinamicamente - liberar a memória no destrutor da classe
+    
+
+public:
+    Image(); //definido durante tempo de execucao  
+    Image(int w, int h);  //pode ser definido no codigo alocando memoria diretamente
+    ~Image();
+    
+    
+    void setHeight(int altura);
+    void setWidth(int largura);
+    int getHeight();
+    int getWidth();
+    void setPixel (int linha, int coluna, Colors cor);
+    Colors getPixel(int linha, int coluna) const;
+    void salvarComoPPM(const string& nomeArquivo) const; //const para a funcao nao alterar atributos da classe & para nao gerar copias acessando diretamente
+
+};
+
+
+#endif
